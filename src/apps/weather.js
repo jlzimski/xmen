da689daca8e5982edaafff1a34a0fdf50f162fb2
Hanna <Bethany.Hanna@autofinance.com>
2021-07-05 23:35:02 -0400

    updated weather.js and got the api call to ticketmaster working

--- src/apps/weather.js
+++ src/apps/weather.js
@@ -1,151 +1,46 @@
 // API KEY = f71dfe271bd670c7684da61f3894c9f6
 // URL = api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 //list.main.temp for current temp & list.weather[0].main for first in array of current conditions
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-//const baseURL = 'https://api.openweathermap.org/data/2.5/onecall?';
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
-const weatherKey = "f71dfe271bd670c7684da61f3894c9f6";
-
+// import Conditions from './conditions/Conditions';
 
 const Weather = (props) => {
-    // const [lat, setLat] = useState('lat');
-    // const [lon, setLon] = useState('lon');
+
     const lat = props.lat;
     const lon = props.lon;
-    const [results, setResults] = useState([]);
-    // const [icon, setIcon] = useState('');
-    // const [temp, setTemp] = useState('');
-    // const [description, setDescription] = useState('');
-    // const [city, setCity] = useState('');
+    console.log(lat, lon);
+    // const [unit, setUnit] = useState('imperial');
+    // const baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
 
-//api.openweathermap.org/data/2.5/find?q=London&units=metric
-    // useEffect(() => {
-    //     let url = `${baseUrl}lat=${lat}&lon=${lon}&exclude=hourly,daily&units=metric&appid=${weatherKey}`; // &units=${units} (Celsius=metric Fahrenheit=imperial)
-    //     console.log(url);
-
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(data => setResults(data))
-    //             console.log(results)
-    //         .catch(err => console.log(err))
-    // }, [lat,lon])
-
-    // if (lat && lon) {
-    //     //let url = `${baseURL}lat=${lat}&lon=${lon}&exclude=hourly,daily&units=metric&appid=${weatherKey}`; // &units
-    //     let url = `${baseURL}lat=${lat}&lon=${lon}&units=metric&appid=${weatherKey}`;
-    //     console.log(url);
-    //     fetch(url)
+    //     const weatherURL =
+    //     `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.owmKey}`
+    // fetch(weatherURL)
     //     .then(res => res.json())
-    //     .then(data => setResults(data))
-    //     .catch(err => console.log(err))
-    //     return;
-    // }
+    //     .then(data => console.log("Data List Loaded", data.list))
 
-    // console.log(results);
-
-
-    // setIcon[results.weather.icon];
-    // setDescription[results.weather.description];
-    // setTemp[results.main.temp];
-    // setCity[results.name];
-
-    
-
-    return(
+    const key = '6ccc78695554c2475d4eac24bbf01d17';
+    const unit = 'imperial'
+    const [feels_like, setFeelsLike] = useState('');
+    const [mainTemp, setMainTemp] = useState('');
+    const [description, setDescription] = useState('');
+    const [main, setMain] = useState('');
+    const [iconID, setIconID] = useState('');
+    const weatherURL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${key}`
+    useEffect(() => {
+        fetch(weatherURL)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        })
+    }, [])
+    return (
         <div>
-            <p>Weather</p>
-            {/* <p>
-            {props.lat}
-            </p>
-            <p>
-            {props.lon}
-            </p> */}
+            <div>
+                
+            </div>
         </div>
     )
-};
+}
 
 export default Weather;
-
-// import React, { Component } from 'react';
-// const weatherApi = `${process.env.REACT_APP_WEATHER_ENDPOINT}`;
-// const weatherApiKey = `${process.env.REACT_APP_WEATHER_API_KEY}`;
-// ​
-// class WeatherApp extends Component {
-//     state = {
-//         lat: undefined,
-//         lon: undefined,
-//         city: undefined,
-//         temperatureC: undefined,
-//         temperatureF: undefined,
-//         icon: undefined,
-//         description: undefined,
-//         errorMessage: undefined,
-//     }
-// ​
-//     getPosition = () => {
-//         return new Promise(function (resolve, reject) {
-//             navigator.geolocation.getCurrentPosition(resolve, reject);
-//         });
-//     }
-// ​
-//     getWeather = async (lat, lon) => {
-//         let url = `${weatherApi}lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`
-//         const api_call = await fetch(url);
-//         const data = await api_call.json();
-//         this.setState({
-//             lat: lat,
-//             lon: lon,
-//             city: data.name,
-//             temperatureC: Math.round(data.main.temp),
-//             temperatureF: Math.round(data.main.temp * 1.8 + 32),
-//             description: data.weather[0].description,
-//         })
-//     }
-// ​
-// ​
-//     componentDidMount() {
-//         this.getPosition()
-//             .then((position) => {
-//                 this.getWeather(position.coords.latitude, position.coords.longitude)
-//             })
-//             .catch((err) => {
-//                 this.setState({ errorMessage: err.message });
-//             });
-// ​
-//         this.timerID = setInterval(
-//             () =>
-//                 this.getWeather(this.state.lat, this.state.lon),
-//             60000
-//         );
-//     }
-// ​
-//     componentWillUnmount() {
-//         clearInterval(this.timerID);
-//     }
-// ​
-//     render() {
-//         const { city, temperatureC, temperatureF, description } = this.state;
-//         if (city) {
-//             return (
-//                 <div>
-                    
-//                         <h1>Weather today in {city}</h1>
-//                         <h2>{temperatureC} &deg;C <span>/</span> {temperatureF} &deg;F</h2>
-//                         <h2>
-                            
-//                             <span>Description: {description}</span>
-                            
-//                         </h2>
-//                 </div>
-//             );
-//         }
-//         else {
-//             return (
-//                 <div>Loading...</div>
-//             )
-//         }
-//     }
-// }
-// ​
-// export default WeatherApp;
